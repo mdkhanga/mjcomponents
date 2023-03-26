7c76b4d2d2e8e901cd2a9725a4b87eef99a4e317
mdkhanga <khangaonkar@gmail.com>
2023-03-26 15:18:56 -0700

    update home page

--- myfinance/src/pages/index.js
+++ myfinance/src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Get started by editing MMMMMMMMMMM&nbsp;
             <code className={styles.code}>src/pages/index.js</code>
           </p>
 	  <p> This is Manoj here </p>
@@ -46,19 +46,8 @@ export default function Home() {
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+          
+        
 
           <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
