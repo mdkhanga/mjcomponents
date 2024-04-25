df0ff73bb24b196cdd786433066506baf23d8aca
mdkhanga <khangaonkar@gmail.com>
2024-04-24 17:29:40 -0700

    monthlybal controller

---
+++ services/src/main/java/com/heavyduty/services/controller/MonthlyBalanceController.java
@@ -0,0 +1,26 @@
+package com.heavyduty.services.controller;
+
+import com.heavyduty.services.api.Account;
+import com.heavyduty.services.api.MonthBalance;
+import com.heavyduty.services.repository.MonthlyBalanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/mbalances")
+@CrossOrigin
+public class MonthlyBalanceController {
+
+    @Autowired
+    MonthlyBalanceRepository monthlyBalanceRepository;
+
+
+    @GetMapping("/{username}/{year}")
+    public List<MonthBalance> getMonthlyBalances(@PathVariable("username") String username,
+                                                 @PathVariable("year") int year) {
+
+        return monthlyBalanceRepository.getMonthlyBalances(username, year);
+    }
+}
